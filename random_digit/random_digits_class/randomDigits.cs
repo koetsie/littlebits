2721e465a668b1c72f7abe1e1ffd5775f7788723
koetsie <commit@littlebits>
2013-05-13 16:36:42 +0200

    Added from development server
    Random Digits generator class + a test Consol application

---
+++ random_digit/random_digits_class/randomDigits.cs
@@ -0,0 +1,191 @@
+﻿
+// The goal is to return a strong pseudo Random Digit between 0 and 9.
+// using the RandomNumberGenerator RNGCryptoServiceProvider from the
+// System.Security.Cryptography Namespace. 
+//
+// if we take the int 0,1,2,3 and pick Randomly 3 x One then the sum of the 
+// three draws wil by a minimum value of 0 and a Maximum value of 9. 
+// 
+// If we just look at the fact if a Byte value is odd or even and use 2 bytes per draw
+// we should get a fair Psuedo Random result. I think :-)
+//
+//====================================================================================
+//  So lets assume we have a Byte Array X={a,b,c,d,e,f} values{1,34,55,147,255,6}
+//
+//           [a,b]  = odd/even   [c,d]          =odd/odd       [e,f]    = odd/even
+//           [1][34]= 3 (r1)     [55][147]      =3  (r2)       [255][6] = 2 (r3)
+//
+// Result: The sum of the 3x2 byte pairs is 3 + 3 + 2 = >>>>> 8
+//=====================================================================================
+//
+// Each round we opposite the Odd/even values. For better distibution
+// This was also done in the Hot Bits Radiation project. Ater testing the 
+// Bell curve was better distributed.
+//
+//
+// first round  2 bytes
+//0	00	25%         if   byte(0) odd     &&    byte(1) odd  =0
+//1	01	25%         if   byte(0) even    &&    byte(1) odd  =1
+//2	10	25%         if   byte(0) even    &&    byte(1) even =2  
+//3	11	25%         if   byte(0) odd     &&    byte(1) even =3 
+//----- > sum1 = [0-3]
+// second  round 2 bytes (notice we reverse the values)
+//0	00	25%         if   byte(0) odd     &&    byte(1) odd  =3
+//1	01	25%         if   byte(0) even    &&    byte(1) odd  =2
+//2	10	25%         if   byte(0) even    &&    byte(1) even =1  
+//3	11	25%         if   byte(0) odd     &&    byte(1) even =0 
+ 
+//----- >sum2 = [0-3]
+// third round  2 byte (notice we reverse the values)
+//0	00	25%         if   byte(0) odd     &&    byte(1) odd  =1
+//1	01	25%         if   byte(0) even    &&    byte(1) odd  =3
+//2	10	25%         if   byte(0) even    &&    byte(1) even =0  
+//3	11	25%         if   byte(0) odd     &&    byte(1) even =2 
+//----- > sum3 =  [0-3]
+//
+//
+// the integer of value between 0-9 is  (sum1 + sum 2 + sum 3)
+//
+// If you like it please mention my name
+// Marc Koutzarov The Netherlands
+// For comments ######
+
+
+
+
+
+
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ps.security
+{
+    public class RandomDigits
+    {
+
+        
+
+        ///<summary>
+        /// Returns One random Digit of a value between 0 and 9.
+        ///</summary>
+        
+        
+        public static int getDigit(){
+            int[] Result = { 0, 0, 0 }; // this stores sum1 , sum2 and sum3 
+            int R = 0; // Will hold the resulting integer Sum of Result{3}
+            int b1 = 0; // byte one
+            int b2 = 0; // byte two 
+            int offset = 0; // need an offset to loop 3 times. Each time i get 2 bytes to compare.
+          
+            //get 6 random bytes
+            RandomNumberGenerator rng = new RNGCryptoServiceProvider();
+            
+            byte[] b = new byte[6];
+             
+            rng.GetNonZeroBytes(b);
+
+
+            for (int i =0; i <=2; i++)  // Loop 3 time  i=2 i=1 and i=0
+               { 
+
+               //get the offset 
+                if (i == 2) {
+                    offset = 4; // 5th and 6th byte 
+                }else if(i == 1) {
+                    offset = 2; // 3th and 4th byte 
+                }else if (i == 0){
+                    offset = 0; // 1th and 2th byte
+                };
+
+
+
+
+                //(b1,2 MOD 2) b1 and b2 will be odd number is 1 and even if 0
+                b1 = ((int)b[offset] % 2);
+                b2 = ((int)b[offset+1] % 2);
+
+
+                if ((i == 0))
+                {
+                    if ((b1 == 1) && (b2 == 1))
+                    {
+                        Result[i] = 0;              //odd-odd   = 0
+                    }
+                    else if ((b1 == 0) && (b2 == 1))
+                    {
+                        Result[i] = 1;              //even-odd  = 1
+                    }
+                    else if ((b1 == 0) && (b2 == 0))
+                    {
+                        Result[i] = 2;              //even-even = 2  
+                    }
+                    else if ((b1 == 1) && (b2 == 0))
+                    {
+                        Result[i] = 3;
+                    };           //odd-even  = 3
+                }
+                else if (i==1) {
+                    // on second round flip the values 
+                    if ((b1 == 1) && (b2 == 1))
+                    {
+                        Result[i] = 3;              //odd-odd   = 3
+                    }
+                    else if ((b1 == 0) && (b2 == 1))
+                    {
+                        Result[i] = 2;              //even-odd  = 2
+                    }
+                    else if ((b1 == 0) && (b2 == 0))
+                    {
+                        Result[i] = 1;              //even-even = 1  
+                    }
+                    else if ((b1 == 1) && (b2 == 0))
+                    {
+                        Result[i] = 0;          //odd-even  = 0
+                    };
+
+                }
+                else if (i == 2) {
+
+                    // on 3 round  flip the values once more
+                    if ((b1 == 1) && (b2 == 1))
+                    {
+                        Result[i] = 1;              //odd-odd   = 1
+                    }
+                    else if ((b1 == 0) && (b2 == 1))
+                    {
+                        Result[i] = 3;              //even-odd  = 3
+                    }
+                    else if ((b1 == 0) && (b2 == 0))
+                    {
+                        Result[i] = 0;              //even-even = 0  
+                    }
+                    else if ((b1 == 1) && (b2 == 0))
+                    {
+                        Result[i] = 2;          //odd-even  = 2
+                    };
+
+                }
+
+
+                b1 = 0;
+                b2 = 0;
+
+            };// end 3 for loops
+
+
+
+
+            //Sum the results of round 1,2 and 3 together
+            //this will make an Int between 0 - 9 
+            R =(int)(Result[0] + Result[1] + Result[2]);
+
+            //Return a Int valued between 0 - 9
+            return R;
+
+        }
+
+
+       
+    }
+}
---
+++ random_digit/testApp_console/Program.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ps.security;
+using System.Diagnostics; 
+
+
+
+
+
+    class Program
+    {
+
+
+
+
+        static void Main(string[] args){
+            
+            
+            
+            Console.WriteLine("");
+            Console.WriteLine("Test nr 1. Testing frequency of appearance");
+            Program.DrawNumbersPerDigitTest();
+            Console.WriteLine("Any key to start next test");
+            Console.ReadKey();
+             
+             Console.WriteLine("");
+             
+            
+            Console.WriteLine("Test 2 draw 10.000 digits");
+            Program.DrawDigitTest(10000);
+             
+            Console.WriteLine("Done. Any key to start next test");
+            Console.ReadKey();
+
+
+
+
+
+             Console.WriteLine("");
+             Console.WriteLine("50 x Random 4 digit pin");
+             for (int i = 50; i >= 1; i--)
+             {
+                 Console.WriteLine(GetPin(4));
+             }
+
+             Console.WriteLine("Done.");
+             Console.ReadKey();  
+         }
+
+
+
+
+        // returns digits as string c is the number of digits
+        public static string GetPin(int c) {
+            StringBuilder b = new StringBuilder();
+
+          
+            for (int i = c; i >= 1; i--)
+            {
+              
+                int n = ps.security.RandomDigits.getDigit();
+                b.Append(n.ToString()); 
+
+            }
+            return b.ToString();
+        }
+
+
+   
+        // Draws a the digits 0-9 
+        // each digit(from 0-9) is being drawn untill it appears
+        // At the end it shows the Total draws needed for all didigts to appear.
+        // and the avg number of draws per digit.
+
+     public static int[] DrawNumbersPerDigitTest()
+      {
+
+            Console.WriteLine("Random digits  0-9 generator test.");
+
+            double frequency = System.Diagnostics.Stopwatch.Frequency;
+            double nanosecPerTick = (1000 * 1000 * 1000) / frequency;
+
+            int totalItter = 0;
+            int[] Totaldistri = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            for (int AllDigits = 9; AllDigits >= 0; AllDigits--)
+            {
+
+
+                Stopwatch timer = new Stopwatch();
+
+                int avg = 0;
+                int itters = 0;
+                int[] distri = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+                timer.Start();
+
+                for (int i = 1000000; i >= 1; i--)
+                {
+
+                    int n = ps.security.RandomDigits.getDigit();
+
+                    avg = (avg + n);
+                    distri[n]++;
+                    itters++;
+                    if (n == AllDigits)
+                    {
+                        break;
+                    }
+                };
+                timer.Stop();
+                
+                double durNanosec = timer.ElapsedTicks * nanosecPerTick;
+                double durSecs = timer.ElapsedTicks * 1000 / frequency;
+                
+                Console.WriteLine("");
+                Console.WriteLine("|----------------------------------|");
+                Console.WriteLine("Digit nr:\t" + AllDigits);
+                Console.WriteLine("");
+                Console.WriteLine("|  int\t|  freq\t percentage\t   |");
+                Console.WriteLine("|----------------------------------|");
+
+                for (int i = 9; i >= 0; i--)
+                {
+                    double p =Math.Round( ((double)distri[i])/((double)itters / 100),3);
+                    Console.WriteLine("|  " + i + "\t|  " + +distri[i] + "\t " + p + "%\t");
+                }
+                Console.WriteLine("|----------------------------------|");
+                Console.WriteLine("");
+                Console.WriteLine("  Sum     : >\t" + avg.ToString());
+                Console.WriteLine("  Average : >\t" + ((double)avg / (double)itters).ToString());
+                Console.WriteLine("  Count   : >\t" + (itters).ToString());
+                Console.WriteLine("");
+                Console.WriteLine("  nSecs   :\t" + Math.Round(durNanosec, 3));
+                Console.WriteLine("  mSecs   :\t" + Math.Round(durSecs, 9));
+
+                totalItter += (itters);
+                Totaldistri[AllDigits] = itters;
+            };
+
+            Console.WriteLine("");
+            Console.WriteLine("|----------------------------------|");
+            Console.WriteLine("Average + total draws per Digit:");
+            Console.WriteLine("|----------------------------------|");
+            Console.WriteLine("");
+            Console.WriteLine("  total draws\t: >\t" + totalItter.ToString());
+            Console.WriteLine("  avg\t\t: >\t" + Math.Round((double)totalItter / 10, 3));
+
+            Console.WriteLine("|  int\t|  freq\t ");
+            Console.WriteLine("|----------------------------------|");
+
+            for (int i = 9; i >= 0; i--)
+            {
+                Console.WriteLine("|  " + i + "\t|  " + +Totaldistri[i] + "\t \t");
+            }
+
+
+            return Totaldistri;
+
+      }
+
+
+
+     // Draws a the digits 0-9 
+     // each digit(from 0-9) is being drawn untill it appears
+     // At the end it shows the Total draws needed for all didigts to appear.
+     // and the avg number of draws per digit.
+
+     public static void DrawDigitTest(int nrOfDigits)
+     {
+
+         Console.WriteLine("Random digits  0-9 generator test.");
+
+         double frequency = System.Diagnostics.Stopwatch.Frequency;
+         double nanosecPerTick = (1000 * 1000 * 1000) / frequency;
+        
+         Stopwatch timer = new Stopwatch();
+
+             int avg = 0;
+             int itters = 0;
+             int[] distri = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+             timer.Start();
+
+             for (int i = nrOfDigits; i >= 1; i--)
+             {
+
+                 int n = ps.security.RandomDigits.getDigit();
+
+                 avg = (avg + n);
+                 distri[n]++;
+                 itters++;
+                 
+             };
+             timer.Stop();
+
+             double durNanosec = timer.ElapsedTicks * nanosecPerTick;
+             double durSecs = timer.ElapsedTicks * 1000 / frequency;
+
+            
+             Console.WriteLine("");
+             Console.WriteLine("| int\t|  freq\t percentage\t   |");
+             Console.WriteLine("|----------------------------------|");
+
+             for (int i = 9; i >= 0; i--)
+             {
+                 double p = Math.Round(((double)distri[i]) / ((double)itters / 100), 1);
+                 Console.WriteLine("| " + i + "\t| " + +distri[i] + "\t\t" + p + "%.");
+             }
+             Console.WriteLine("|----------------------------------|");
+             Console.WriteLine("");
+             Console.WriteLine("  #Digits : >\t\t" + (itters).ToString());
+             Console.WriteLine("  Sum     : >\t\t" + avg.ToString());
+             Console.WriteLine("  Average : >\t\t" + ((double)avg / (double)itters).ToString());
+             
+             Console.WriteLine("");
+             Console.WriteLine("  nSecs   : >\t" + Math.Round(durNanosec, 3));
+             Console.WriteLine("  mSecs   : >\t" + Math.Round(durSecs, 9));
+     
+     }
+
+
+       
+
+     }
+
+
+        
+
+
+    
+
